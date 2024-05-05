a3c6b886e5cab440155f5c8e57c4cab71eedc1f5
Rishijay Shrivastava <rishijay.shrivastava@gmail.com>
2024-05-05 17:26:09 +0530

    Added routes

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// routes
+app.use("/employees", require("./routes/routes"));
 app.get("/", (req, res) => {
-  res.send("Hello world.!!");
+  res.send("Welcome to basic NodeJS Express based api project.!");
 });
 
 app.listen(port, () => {
